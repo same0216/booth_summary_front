f70658a268578bdd601e88f2d35476dd336a5ddd
same0216 <ashida0216@yahoo.co.jp>
2023-07-08 03:05:49 +0900

    [add]Chakra UI・新規登録画面 追加

---
+++ app/register/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+
+import axios from "axios";
+import { Flex, Heading, Input, Button, Center, FormControl, FormErrorMessage } from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+
+
+export default function Register() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting }
+  } = useForm()
+
+  const onsubmit = async (data) => {
+    await axios({
+      method: "post",
+      url: "https://5573.me/users/register",
+      data: {username: data.user, password: data.password}
+    })
+
+    .then((result => {
+      console.log("成功", result);
+    }))
+    
+    .catch((error => {
+      console.log("失敗", error);
+    }))
+  } 
+
+  return(
+    <Flex height="100vh" alignItems="center" justifyContent="center">
+      <Flex direction="column" background="gray.100" p={12} rounded={6}>
+        <Heading mb={6} textAlign="center">新規登録</Heading>
+          <form onSubmit={handleSubmit(onsubmit)}>
+            <FormControl isInvalid={errors.user}>
+              <Input 
+                id="user"
+                placeholder="ユーザーID" 
+                variant="filled" 
+                type="text"
+                mt={6}
+                {...register('user',{
+                  required: "ユーザーIDを入力してください。",
+                  minLength: { value: 4, message: "4文字以上で入力してください。"},
+                  pattern: { value: /^[0-9a-zA-Z]*$/, message: "ユーザー名は半角英数字で入力してください。"}
+                })} 
+              />
+              <FormErrorMessage>
+                {errors.user && errors.user.message}
+              </FormErrorMessage>
+            </FormControl>
+            <FormControl isInvalid={errors.password}>
+              <Input 
+                id="password"
+                placeholder="パスワード"
+                variant="filled"
+                type="password"
+                mt={6}
+                {...register('password',{
+                  required: "パスワードを入力してください。",
+                  minLength: { value: 6, message: "6文字以上で入力してください。"},
+                  maxLength: { value: 32, message: "32文字以内で入力してください。"},
+                  pattern: { value: /^[0-9a-zA-Z]*$/, message: "パスワードは半角英数字で入力してください。"}
+                })} 
+              />
+              <FormErrorMessage>
+                {errors.password && errors.password.message}
+              </FormErrorMessage>
+            </FormControl>
+            <Center>
+              <Button mt={6} colorScheme="teal" isLoading={isSubmitting} type="submit">送信</Button>
+            </Center>
+          </form>
+          
+      </Flex>
+    </Flex>
+  );
+}
